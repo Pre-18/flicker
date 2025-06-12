3f5402e69928f550f900ffb55b4b5ee5e1626a19
Pre-18 <emailtanjiro7@gmail.com>
2025-06-12 09:19:22 +0530

    user routes declaration

--- src/app.js
+++ src/app.js
@@ -9,10 +9,18 @@ app.use(cors({
   origin: process.env.CORS_ORIGIN,
   credentials: true,
 }));
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(cookieParser());
+
+//routes
+import userRouter from './routes/user.routes.js';
+
+//routes declaration
+app.use('/api/v1/users', userRouter);
+//http://localhost:8000/api/v1/users/register
 
 export { app };
