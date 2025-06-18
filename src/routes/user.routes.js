39520d2e59c44d8af83479d8b1373f64662b2268
Pre-18 <emailtanjiro7@gmail.com>
2025-06-19 00:19:58 +0530

    added login and logut routes and logic

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser,registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 
 const router= Router();
@@ -12,6 +12,9 @@ router.route('/register').post(
   registerUser
 )
 
-//router.route("/login").post(loginUser)
+router.route("/login").post(loginUser)
+//secure routes
+router.route("/logout").post(verifyJWT,logoutUser);
+
 
 export default router;
